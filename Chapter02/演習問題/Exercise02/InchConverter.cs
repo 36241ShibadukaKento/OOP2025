e41b8929ce2d92defec673716d3542dc37b809ab
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-04-22 11:40:35 +0900

    インチからメートル変換完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class InchConverter
+    {  public static double InchToMeter(double inch) {
+                return inch * 0.0254;
+            }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = InchConverter.InchToMeter(inch);
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+
+            }
+          
+        }
+    }
+}
+
+
+
