b06edbc6e59969a5d4717e2acb5a712de79f95a2
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-04-23 09:29:48 +0900

    機能追加①完成

--- Chapter02/演習問題/Exercise02/InchConverter.cs
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise02
-{
-    class InchConverter
-    {  public static double InchToMeter(double inch) {
-                return inch * 0.0254;
-            }
+namespace Exercise02 {
+    class InchConverter {
+        public static double InchToMeter(double inch) {
+            return inch * 0.0254;
+        }
+        public static double MeterToInch(double meter) {
+            return meter / 0.0254;
+        }
     }
 }
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -3,13 +3,28 @@
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            Console.WriteLine("1 : Inch から Meter");
+            Console.WriteLine("2 : Meter から Inch");
+            Console.Write(">");
+            int rule = int.Parse(Console.ReadLine());
 
-            for (int inch = 1; inch <= 10; inch++) {
-                double meter = InchConverter.InchToMeter(inch);
-                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            Console.Write("start : ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("end : ");
+            int end = int.Parse(Console.ReadLine());
+            if (rule == 1) {
+                for (int inch = start; inch <= end; inch++) {
+                    double meter = InchConverter.InchToMeter(inch);
+                    Console.WriteLine($"{inch}inch = {meter:0.0000}m");
 
+                }
+            } else if (rule == 2) {
+                for (int meter = start; meter <= end; meter++) {
+                    double inch = InchConverter.MeterToInch(meter);
+                    Console.WriteLine($"{meter}m = {inch:0.0000}inch");
+
+                }
             }
-          
         }
     }
 }
