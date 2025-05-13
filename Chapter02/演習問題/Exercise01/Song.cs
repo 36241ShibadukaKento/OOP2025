e4d73507fa1e3ee6a1d9ba6d70f73ffc9dbf604d
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-05-13 10:10:40 +0900

    2.1.1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{   //2 .1 .1
+   public class Song
+    {
+        String Title ;
+        String ArtistName ;
+        int Length;
+
+    }
+    //2 .1 .2
+}
