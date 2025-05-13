156a9521243589521e83be405083fd0be2b510ec
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-05-13 10:29:12 +0900

    2.1.2完成と2.1.1の修正

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -2,10 +2,11 @@
     public class Program {
         static void Main(string[] args) {
             //2 .1 .3
+
         }
-            //2 .1 .4
-            private static void printSongs(Song[] songs) {
-            
+        //2 .1 .4
+        private static void printSongs(Song[] songs) {
+
         }
-   }
+    }
 }
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01
-{   //2 .1 .1
-   public class Song
-    {
-        String Title ;
-        String ArtistName ;
-        int Length;
+namespace Exercise01 {   //2 .1 .1
 
+    public class Song {
+        public String Title { get; set; } = String.Empty;
+        public String ArtistName { get; set; } = string.Empty;
+        public int Length { get; set; }
+
+        //2 .1 .2
+        public Song(String Title, String ArtistName, int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+
+        }
     }
-    //2 .1 .2
+
+
 }
