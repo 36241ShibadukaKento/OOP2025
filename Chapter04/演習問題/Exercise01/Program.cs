77509577760883df0779de05dfab9cf84b602d5d
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-05-23 15:26:03 +0900

    4.2.1

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -48,8 +48,7 @@ namespace Exercise01 {
 
         private static void Exercise3(List<string> langs) {
             //2行で完成させる
-            var lang = langs.Find(s => s.Length >= 10) ?? ("unknown");
-            Console.WriteLine(lang);
+            Console.WriteLine(langs.Find(s => s.Length == 10) ?? ("unknown"));
         }
     }
 }
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+
+        }
+
+        private static void Exercise1() {
+            Console.Write("変換前の数値 : ");
+            var line = Console.ReadLine();
+           if ( int.TryParse(line, out var num)) {
+                if (num < 0) {
+                    Console.WriteLine(num);
+                } else if (num < 100) {
+                    Console.WriteLine(num * 2);
+                } else if (num < 500) {
+                    Console.WriteLine(num * 3);
+                } else {
+                    Console.WriteLine(num);
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+
+            }
+
+        }
+
+        private static void Exercise2() {
+        }
+
+        private static void Exercise3() {
+        }
+    }
+}
