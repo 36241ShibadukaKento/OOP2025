6d657468b69a6d37ae0faace00d4261292d3602e
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-05-23 13:52:44 +0900

    4.1.1完成（繰り返し処理の前にラムダ式を用いてリストに格納）

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Runtime.InteropServices;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
@@ -15,11 +17,27 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            var includeS = langs.Where(i => i.Contains("S")).ToList();
+            foreach (var cnt in includeS) {
+                Console.WriteLine(cnt);
+            }
+
+            for (int cnt = 0; cnt < includeS.Count; cnt++) {
+                Console.WriteLine(includeS[cnt]);
+            }
+
+            int whileCnt = 0;
+            while (true) {
+                Console.WriteLine(includeS[whileCnt]);
+                whileCnt++;
+                if (whileCnt >= includeS.Count) {
+                    break;
+                }
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-       
+
         }
 
         private static void Exercise3(List<string> langs) {
