8141e96688ad5d67354f72224e32003be2654ad5
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-05-23 15:41:57 +0900

    4.2.3

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -15,7 +15,7 @@ namespace Exercise02 {
         private static void Exercise1() {
             Console.Write("変換前の数値 : ");
             var line = Console.ReadLine();
-           if ( int.TryParse(line, out var num)) {
+            if (int.TryParse(line, out var num)) {
                 if (num < 0) {
                     Console.WriteLine(num);
                 } else if (num < 100) {
@@ -33,9 +33,43 @@ namespace Exercise02 {
         }
 
         private static void Exercise2() {
+            Console.Write("変換前の数値 : ");
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                switch (num) {
+                    case < 0:
+                        Console.WriteLine(num);
+                        break;
+                    case < 100:
+                        Console.WriteLine(num * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(num * 3);
+                        break;
+                    default:
+                        Console.WriteLine(num);
+                        break;
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
 
+
         private static void Exercise3() {
+            Console.Write("変換前の数値 : ");
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                var text = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num + 3,
+                    _ => num
+                };
+                Console.WriteLine(text);
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
     }
 }
