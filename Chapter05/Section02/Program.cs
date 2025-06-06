b163b05aa15da790263ea05d775c8856ae63f281
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-06-06 15:27:45 +0900

    5.1.2 完成

---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var appVar1 = new AppVersion(5, 1, 3);
+            var appVar2 = new AppVersion(5, 1, 3);
+
+            if (appVar1 == appVar2) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+
+    public record AppVersion(int major, int minor, int build = 0, int revision = 0) {
+        public int Major { get; init; } = major;
+        public int Minor { get; init; } = minor;
+        public int Build { get; init; } = build;
+        public int Revision { get; init; } = revision;
+
+
+        public override string ToString() =>
+            $"{Major}.{Minor}.{Build}.{Revision}";
+
+    }
+}
+
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    public class YearMonth {
+        //5.1.1
+        public int Year { get; init; }
+        public int Month { get; init; }
+        public  YearMonth(int y ,int m) {
+             Year = y;
+             Month = m;
+        }
+        //5.1.2
+        public bool Is21Century {
+            get{
+                return 2000 < Year && Year < 2100;
+            }
+        }
+    }
+}
