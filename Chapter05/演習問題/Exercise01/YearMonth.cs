f6793ed2f691f42f4b2bfbf96585937fe5645496
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-06-06 15:42:46 +0900

    5.1.3 完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01
-{
+namespace Exercise01 {
     public class YearMonth {
         //5.1.1
         public int Year { get; init; }
         public int Month { get; init; }
-        public  YearMonth(int y ,int m) {
-             Year = y;
-             Month = m;
+        public YearMonth(int y, int m) {
+            Year = y;
+            Month = m;
         }
         //5.1.2
         public bool Is21Century {
-            get{
+            get {
                 return 2000 < Year && Year < 2100;
+            }
+        }
+        //5.1.3
+        public YearMonth AddOneMonth() {
+            if(Month == 12) {
+                return new YearMonth(Year + 1 , 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
             }
         }
     }
