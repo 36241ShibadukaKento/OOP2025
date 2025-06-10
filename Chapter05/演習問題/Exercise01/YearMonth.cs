1f4c1e78c26b655c27a2972849977d43b4a0d828
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-06-10 11:59:51 +0900

    5.3 完成 (答え合わせ後)

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -6,24 +6,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
-    public class YearMonth(int y, int m) {
-        //5.1.1
-        //プライマリーコンストラクタ
-        public int Year { get; init; } = y;
-        public int Month { get; init; } = m;
 
+    //5.1.1
+    //プライマリーコンストラクタ
+    public record YearMonth(int Year, int Month) {
         //5.1.2
         public bool Is21Century => 2000 < Year && Year <= 2100;
 
         //5.1.3
         public YearMonth AddOneMonth() {
-            if(Month == 12) {
-                return new YearMonth(Year + 1 , 1);
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
             } else {
                 return new YearMonth(Year, Month + 1);
             }
         }
         //5.1.4
-        public override string ToString() => $"<< { Year }年 { Month }月 >>";
+        public override string ToString() => $"<< {Year}年 {Month}月 >>";
     }
 }
