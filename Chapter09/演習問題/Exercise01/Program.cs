68d8c4d27a932912726c4c4f53b14ffcc697b827
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-06-27 11:41:12 +0900

    9.2 完成

--- Chapter09/演習問題/Exercise01/Program.cs
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 
 namespace Exercise01 {
@@ -15,14 +16,14 @@ namespace Exercise01 {
         private static void DisplyDatePattern1(DateTime dateTime) {
             // 2024/03/09 19:03
             // string.Format
-            var format =  string.Format("{0:yyyy/MM/dd HH:mm}",dateTime);
+            var format =  string.Format($"{dateTime:yyyy/MM/dd HH:mm}");
             Console.WriteLine(format);
         }
 
         private static void DisplyDatePattern2(DateTime dateTime) {
             // 2024年03月09日 19時03分09秒
             // DateTime.ToString を使った例
-            var format1 = dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒");
+            var format1 = dateTime.ToString($"{dateTime:yyyy年MM月dd日 HH時mm分ss秒}");
             Console.WriteLine(format1);
         }
 
@@ -31,10 +32,13 @@ namespace Exercise01 {
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
 
             var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
-            var format2 = dateTime.ToString("ggyy年M月d日", culture);
-            Console.WriteLine($"{format2} ({dayOfWeek})");
-     
+            var format2 = dateTime.ToString("ggy", culture);
+            Console.WriteLine($"{format2}年{dateTime.Month,2}月{dateTime.Day,2}日 {dateTime.Hour,2}時 {dateTime.Minute,2}分({dayOfWeek})");
 
+            var format3 = dateTime.ToString("gg", culture);
+            var year = int.Parse(dateTime.ToString("yy", culture));
+
+            
 
         }
     }
