ccb4c3f56b498b5affe9c5d05caf16220911b7d8
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-06-27 13:47:53 +0900

    第9章完成

--- Chapter09/演習問題/Exercise03/Program.cs
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -1,12 +1,30 @@
 ﻿namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
-            var tw = new TimeWatch();
-            tw.Start();
-            // スリープする
-            Thread.Sleep(1000);
-            TimeSpan duration = tw.Stop();
-            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            Console.WriteLine("計測開始");
+
+            Thread.Sleep(10000);
+
+            sw.Stop();
+            Console.WriteLine("計測終了");
+
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine($"{sw.ElapsedMilliseconds}ミリ秒");
+
+
+
+            //var tw = new TimeWatch();
+
+
+            //tw.Start();
+            //// スリープする
+            //Thread.Sleep(1000);
+            //TimeSpan duration = tw.Stop();
+            //Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
 
         }
     }
