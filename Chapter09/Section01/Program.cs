3c5155a0c66d9beb27ee6210b97ddc0a3c2b0c58
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-06-27 10:07:36 +0900

    9.1 完成

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //var today = new DateTime(2025,7,12);     //日付
+            //var now = DateTime.Now;  //日付と時刻
+
+            //Console.WriteLine($"Today:{ today.Month }");
+            //Console.WriteLine($"Now:{ now }");
+
+
+            //自分の生まれた生年月日の曜日を求める
+            Console.WriteLine("生年月日を入力");
+            Console.Write("西暦:");
+            string? year = Console.ReadLine();
+            Console.Write("月:");
+            string? month = Console.ReadLine();
+            Console.Write("日:");
+            string? day = Console.ReadLine();
+            var birthday = DateTime.Parse(year + "," + month+ "," + day);
+
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+     
+
+            var str = birthday.ToString("ggyy年M月d日", culture);
+            var dayofweek = culture.DateTimeFormat.GetDayName(birthday.DayOfWeek);
+
+            Console.WriteLine(str + dayofweek);
+            Console.WriteLine();
+
+            //生まれてからの日数を求める
+            TimeSpan diff = DateTime.Today - birthday;
+            Console.WriteLine($"生まれてから{diff.TotalDays}日です");
+
+            //年齢を求める
+            int age = GetAge(birthday, DateTime.Today);
+            Console.WriteLine();
+    
+            //1月1日からの経過日数
+            TimeSpan diff1st = birthday - DateTime.Today;
+            Console.WriteLine($"1月1日から{diff1st.TotalDays}日経過");
+            Console.WriteLine();
+
+            //うるう年の判定
+            //西暦の入力
+            var seireki = Console.Read();
+            var LeapYear = DateTime.IsLeapYear(seireki);
+            if (LeapYear) {
+                Console.WriteLine("うるう年です");
+            } else {
+                Console.WriteLine("うるう年ではありません");
+            }
+
+            static int GetAge (DateTime birthday ,DateTime targetDay) {
+                var age = targetDay.Year - birthday.Year;
+                
+                return age;
+            }
+        }
+    }
+}
---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplyDatePattern1(dateTime);
+            DisplyDatePattern2(dateTime);
+            DisplyDatePattern3(dateTime);
+
+
+        }
+
+        private static void DisplyDatePattern1(DateTime dateTime) {
+            // 2024/03/09 19:03
+            // string.Format
+            var format =  string.Format("{0:yyyy/MM/dd HH:mm}",dateTime);
+            Console.WriteLine(format);
+        }
+
+        private static void DisplyDatePattern2(DateTime dateTime) {
+            // 2024年03月09日 19時03分09秒
+            // DateTime.ToString を使った例
+            var format1 = dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒");
+            Console.WriteLine(format1);
+        }
+
+        private static void DisplyDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            var format2 = dateTime.ToString("ggyy年M月d日", culture);
+            Console.WriteLine($"{format2} ({dayOfWeek})");
+     
+
+
+        }
+    }
+}
