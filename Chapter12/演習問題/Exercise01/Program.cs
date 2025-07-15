aa472056f21cc2b0b23548fb9c9dc20cd5475d9f
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-07-15 14:06:28 +0900

    12.1.2 12.1.3 完成

--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -4,19 +4,42 @@ using System.Text.Encodings;
 using System.Xml;
 using System.Xml.Serialization;
 using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
 
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            //問題12.1.1
             var emp = new Employee {
-                Id = 100,
-                Name = "柴塚健斗",
-                HireDate = new DateTime(2005, 7, 19)
+                Id = 123,
+                Name = "山田太郎",
+                HireDate = new DateTime(2018, 10, 1),
             };
             var jsonString = Serialize(emp);
             Console.WriteLine(jsonString);
             var obj = Deserialize(jsonString);
             Console.WriteLine(obj);
+
+            //問題12.1.2
+            Employee[] employees = [
+                new () {
+                    Id = 123,
+                    Name = "山田太郎",
+                    HireDate = new DateTime(2018, 10, 1),
+                },
+                new () {
+                    Id = 198,
+                    Name = "田中華子",
+                    HireDate = new DateTime(2020, 4, 1),
+                },
+            ];
+            Serialize("employees.json", employees);
+
+            //問題12.1.3
+            var empdata = Deserialize_f("employees.json");
+            foreach (var empd in empdata)
+                Console.WriteLine(empd);
+
         }
 
 
@@ -30,22 +53,48 @@ namespace Exercise01 {
             return JsonSerializer.Serialize(emp, options);
         }
 
-    
 
-        static Employee? Deserialize (string text) {
+
+        static Employee? Deserialize(string text) {
             var options = new JsonSerializerOptions {
                 WriteIndented = true,
                 Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             };
-            var novelist = JsonSerializer.Deserialize<Employee>(text,options);
+            var novelist = JsonSerializer.Deserialize<Employee>(text, options);
             return novelist;
         }
 
-        public record Employee {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public DateTime HireDate { get; set; }
+        //問題12.1.2
+        //シリアル化してファイルへ出力する
+        static void Serialize(string filePath, IEnumerable<Employee> employees) {
+            var options = new JsonSerializerOptions {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            string jsonString = JsonSerializer.Serialize(employees, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        //問題12.1.3
+        //逆シリアル化してファイルへ出力する
+        static Employee[] Deserialize_f(string filePath) {
+            var options = new JsonSerializerOptions {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            var text = File.ReadAllText(filePath);
+            var novelist = JsonSerializer.Deserialize<Employee[]>(text, options);
+            return novelist;
         }
     }
+
+    public record Employee {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime HireDate { get; set; }
+    }
 }
+
