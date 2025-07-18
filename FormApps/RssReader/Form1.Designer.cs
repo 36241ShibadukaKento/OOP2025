ffc9d57576169147b1cad4c0359f6f04abdef397
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-07-18 14:10:04 +0900

    Rssリーダー タイトルからページの表示 完成

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -26,10 +26,13 @@
             tbUrl = new TextBox();
             btRssGet = new Button();
             lbTitles = new ListBox();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             SuspendLayout();
             // 
             // tbUrl
             // 
+            tbUrl.BackColor = SystemColors.ActiveCaption;
             tbUrl.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
             tbUrl.Location = new Point(12, 12);
             tbUrl.Name = "tbUrl";
@@ -38,36 +41,53 @@
             // 
             // btRssGet
             // 
+            btRssGet.BackColor = SystemColors.ButtonFace;
             btRssGet.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
             btRssGet.Location = new Point(485, 12);
             btRssGet.Name = "btRssGet";
             btRssGet.Size = new Size(75, 29);
             btRssGet.TabIndex = 1;
             btRssGet.Text = "取得";
-            btRssGet.UseVisualStyleBackColor = true;
+            btRssGet.UseVisualStyleBackColor = false;
             btRssGet.Click += btRssGet_Click;
             // 
             // lbTitles
             // 
             lbTitles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbTitles.BackColor = SystemColors.ActiveBorder;
             lbTitles.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
             lbTitles.FormattingEnabled = true;
             lbTitles.ItemHeight = 20;
             lbTitles.Location = new Point(12, 57);
             lbTitles.Name = "lbTitles";
-            lbTitles.Size = new Size(548, 364);
+            lbTitles.Size = new Size(547, 184);
             lbTitles.TabIndex = 2;
+            lbTitles.Click += lbTitles_Click;
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.BackColor = SystemColors.AppWorkspace;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(12, 260);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(548, 351);
+            webView21.TabIndex = 3;
+            webView21.ZoomFactor = 1D;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(578, 450);
+            ClientSize = new Size(577, 618);
+            Controls.Add(webView21);
             Controls.Add(lbTitles);
             Controls.Add(btRssGet);
             Controls.Add(tbUrl);
             Name = "Form1";
             Text = "RSSリーダ";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -77,5 +97,6 @@
         private TextBox tbUrl;
         private Button btRssGet;
         private ListBox lbTitles;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,28 +1,39 @@
 using System.Net;
+using System.Security.Policy;
 using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
 
         private List<ItemData> items;
-        
+
         public Form1() {
             InitializeComponent();
         }
 
-        private void btRssGet_Click(object sender, EventArgs e) {
-            using (var wc = new WebClient()) { 
-                var url = wc.OpenRead(tbUrl.Text);
-                XDocument xdoc = XDocument.Load(url);   //RSSの取得
+        private async void btRssGet_Click(object sender, EventArgs e) {
+
+            using (var hc = new HttpClient()) {
+                XDocument xdoc = XDocument
+                .Parse(await hc.GetStringAsync(tbUrl.Text));
 
                 //RSSを解析して必要な要素を取得
-                items = xdoc.Root.Descendants("item").
-                    Select(x => new ItemData { Title = (string)x.Element("title"),}).
-                    ToList();
+                items = xdoc.Root.Descendants("item")
+                     .Select(x => new ItemData {
+                         Title = (string?)x.Element("title"),
+                         Link = (string?)x.Element("link"),
+                     }).ToList();
             }
-            foreach (var i in items) {
-                lbTitles.Items.Add(i.Title);         
-            }
+
+            //リストボックスに表示
+            lbTitles.Items.Clear();
+            items.ForEach(item => lbTitles.Items.Add(item.Title));
+        }
+
+        //タイトルからページの表示
+        private void lbTitles_Click(object sender, EventArgs e) {
+            int listNum = lbTitles.SelectedIndex;
+            webView21.Source = new Uri(items[listNum].Link);
         }
     }
 }
