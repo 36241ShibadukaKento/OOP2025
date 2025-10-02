069e5581c1d900ee787b1431146ea9ba5a8ad532
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-10-02 14:30:01 +0900

    InotifyPropertyChangedの場合

---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel() {
+            ChangeMassageCommand = new DelegateCommand(
+                ()=> GreetingMassage = "Bye-Bye World"
+                );
+        }
+
+        private string _greetingMassage = "Hello World!";
+        public string GreetingMassage {
+            get => _greetingMassage;
+            set {
+                if (_greetingMassage != value) {
+                    _greetingMassage = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GreetingMassage)));
+                }
+            }
+        }
+
+        public DelegateCommand ChangeMassageCommand { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
