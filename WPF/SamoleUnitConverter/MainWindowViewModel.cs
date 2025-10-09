1c652b731a17375dc6dcc887df7fefe262a2e56c
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-10-09 13:38:27 +0900

    単位変換アプリ 完成 Prism変更前

---
+++ WPF/SamoleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using SampleUnitConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SamoleUnitConverter
+{
+     internal class MainWindowViewModel : ViewModel
+    {
+        //フィールド
+        private double metricValue;
+        private double imperialValue;
+
+        //▲で呼ばれるコマンド
+        public ICommand ImperialUnitToMetric { get; private set; }
+        //▼で呼ばれるコマンド
+        public ICommand MetricToImperialUnit { get; private set; }
+
+        //上のコンボボックスで選択されている値を保存
+        public MetricUnit CurrentMetricUnit { get; set; }
+        //下のコンボボックスで選択されている値を保存
+        public ImperialUnit CurrentImperialUnit { get; set; }
+
+        //プロパティ
+        public double MetricValue {
+            get => metricValue; 
+            set {
+                this.metricValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public double ImperialValue {
+            get => imperialValue;
+            set {
+                this.imperialValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel() {
+            ImperialUnitToMetric = new DelegateCommand(
+                () =>MetricValue = 
+                CurrentMetricUnit.FromImperialUnit(CurrentImperialUnit,ImperialValue));
+            MetricToImperialUnit = new DelegateCommand(
+                () => ImperialValue = 
+                CurrentImperialUnit.FromMetricUnit(CurrentMetricUnit, metricValue));
+        }
+    }
+}
