65ce3fef26fdaba10f5ef333682ad66140329a03
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-10-30 15:17:48 +0900

    コード14.9

--- Chapter14/Section03/Form1.cs
+++ Chapter14/Section03/Form1.cs
@@ -9,10 +9,21 @@ namespace Section03 {
 
         private async void button1_Click(object sender, EventArgs e) {
             toolStripStatusLabel1.Text = "";
-            await DoLongTimeWork();
-            toolStripStatusLabel1.Text = "終了";
+            var elapsed = await DoLongTimeWorkAsync(4000);
+            toolStripStatusLabel1.Text = $"{elapsed}ミリ秒";
         }
 
+
+        private async Task<long> DoLongTimeWorkAsync(int miliseconds) {
+            var sw = Stopwatch.StartNew();
+            await Task.Run(() => {
+                System.Threading.Thread.Sleep(miliseconds);
+            });
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+
+        //非同期メソッド
         private async Task DoLongTimeWork() {
             await Task.Run(() => {
                 System.Threading.Thread.Sleep(5000);
