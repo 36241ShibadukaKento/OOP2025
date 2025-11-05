38f725727f0a4953f07f75bb1d2dbe247b2b9854
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-05 11:16:41 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hC = new HttpClient();
+            await GetHtmlExample(hC); 
+        }
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://baseball.yahoo.co.jp/mlb/player/2100825/rs";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
