888d64c0cbb9bf783607cee9d857c0079a348a4f
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-10 14:06:01 +0900

    10.1 ②File.ReadAllLinesに変更 完成

--- Chapter10/演習問題/Exercise01/Program.cs
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -4,19 +4,10 @@ namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
             var text = "C://Users//infosys//source//repos//OOP2025//Chapter10//演習問題//Exercise01//source.txt";
+            var target = " class ";
             using (StreamReader reader = new StreamReader(text)) {
-                var cnt = 0;
-                var target = "class";
-                while(!reader.EndOfStream){
-                    var line = reader.ReadLine() ;
-                    List<string> wards = line.Split(' ').ToList();
-                    foreach (var i in wards) {
-                        if (i == target) {
-                            cnt++;
-                        }
-                    }
-                }
-                Console.WriteLine(cnt);
+                var count = File.ReadAllLines(text).Count(s => s.Contains(target));
+                Console.WriteLine(count);
             }
         }
     }
