349022af07a98eed6b35ae766d1560570bee488e
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-10 13:59:11 +0900

    10.1 ① 完成

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "C://Users//infosys//source//repos//OOP2025//Chapter10//演習問題//Exercise01//source.txt";
+            using (StreamReader reader = new StreamReader(text)) {
+                var cnt = 0;
+                var target = "class";
+                while(!reader.EndOfStream){
+                    var line = reader.ReadLine() ;
+                    List<string> wards = line.Split(' ').ToList();
+                    foreach (var i in wards) {
+                        if (i == target) {
+                            cnt++;
+                        }
+                    }
+                }
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
