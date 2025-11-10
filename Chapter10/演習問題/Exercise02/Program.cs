dd649d2c0520b273923c9e15d5b9a09b284fe811
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-10 14:46:56 +0900

    10.2 完成

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "C://Users//infosys//source//repos//OOP2025//Chapter10//演習問題//Exercise02//selfIntroduction.txt";
+            var newFilePath = "C://Users//infosys//source//repos//OOP2025//Chapter10//演習問題//Exercise02//newfile.txt";
+            using (StreamReader reader = new StreamReader(text)) {
+                var lines = File.ReadLines(text).Select((s, ix) => $"{ix + 1,4}:{s}");
+                foreach (var line in lines) {
+                    Console.WriteLine(line);
+                }
+                File.WriteAllLines(newFilePath, lines);
+            }
+        }
+    }
+}
