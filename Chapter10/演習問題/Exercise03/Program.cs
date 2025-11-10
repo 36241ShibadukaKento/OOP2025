307d626adea84ae40df5c3eaace694770765d5b0
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-10 15:53:14 +0900

    10.3 完成

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("text1:");
+            var text1 = Console.ReadLine();
+            //C://Users//infosys//source//repos//OOP2025//Chapter10//演習問題//Exercise03//text1.txt
+
+            Console.Write("text2:");
+            var text2 = File.ReadAllLines(Console.ReadLine());
+            //C://Users//infosys//source//repos//OOP2025//Chapter10//演習問題//Exercise03//text2.txt
+
+            using (var writer = new StreamWriter(text1, append: true)) {
+                foreach (var line in text2) {
+                    writer.WriteLine(line);
+                }
+            }
+
+        }
+    }
+}
