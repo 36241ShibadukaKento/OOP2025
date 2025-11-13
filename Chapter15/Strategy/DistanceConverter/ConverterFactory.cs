30b288efc87eaf74361d00dd7f75cbe18b2150c1
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-13 15:56:20 +0900

    strategyパターン

---
+++ Chapter15/Strategy/DistanceConverter/ConverterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public class ConverterFactory {
+        //あらかじめインスタンスを生成し、配列に入れておく
+        public readonly static ConverterBase[] _converters = {
+            new MeterConverter(),
+            new FeetConverter(),
+            new YardConverter(),
+            new InchConverter()
+        };
+        public static ConverterBase? GetInstance(string name) =>
+            _converters.FirstOrDefault(x => x.IsMyUnit(name));
+    }
+}
