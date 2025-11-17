3ccd1c3770d93ddb1cbba3e0e7abe4be4076be4e
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-17 15:06:15 +0900

    依存性の注入パターン

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TextFileProcessorDI {
+    internal class Program {
+        static void Main(string[] args) {
+            var service = new LineCounterService();
+            var prcessor = new TextFileProcessor(service);
+            Console.WriteLine("パスの入力");
+
+            prcessor.Run(Console.ReadLine());
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/TextFileProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class TextFileProcessor {
+        private ITextFileService _servise;
+
+        //コンストラクタ
+        public TextFileProcessor(ITextFileService servise) {
+            _servise = servise;
+        }
+
+        public void Run(string fileName) {
+            _servise.Initialize(fileName);
+
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines) {
+                _servise.Excute(line);
+            }
+            _servise.Terminate();
+        }
+    }
+}
