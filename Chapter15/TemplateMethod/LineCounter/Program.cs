fbd4599316fab430d2c705fad707cd2f8170cc0b
36241ShibadukaKento <oibc36241@ojs.ac.jp>
2025-11-27 13:53:27 +0900

    パス指定の単語カウント

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -2,6 +2,7 @@
 namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
+            //コメントが異なっていたため念のため再提出（templateMethod指定したパスの読み込み完成）
             Console.Write("ファイルのパスを入力 : ");
             var filePath = Console.ReadLine();
 
